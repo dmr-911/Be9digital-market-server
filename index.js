f323bd5e09da8ba74614cb62f86b3be4df96fa44
mizan <dewan.mizanur911@gmail.com>
2021-12-22 16:40:08 +0600

    initialized with index.js and e_products get method

---
+++ index.js
@@ -0,0 +1,49 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 5000;
+require('dotenv').config();
+const cors = require('cors');
+const { MongoClient } = require("mongodb");
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Mongodb uri and client
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.jycgq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+async function run(){
+    try{
+        await client.connect();
+        const database = client.db("be9digital");
+        const electric_products = database.collection("electric_products");
+        const glasses = database.collection("glass_collection");
+
+        //GET Methods
+        app.get('/e_products', async (req, res) => {
+            const search = req.query.home;
+            let cursor;
+            if(search){
+                cursor = electric_products.find({}).limit(12);
+            }
+            else{
+                cursor = electric_products.find({});
+            }
+            const products = await cursor.toArray();
+            res.send(products);
+        });
+    }
+    finally{
+        // await client.close();
+    }
+};
+run().catch(console.dir);
+
+app.get('/', (req, res)=>{
+    res.send('Hello World!')
+});
+
+app.listen(port, ()=>{
+    console.log(`The server running at port : ${port}`)
+})
