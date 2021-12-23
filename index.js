a398ba10d6f6ab34b49ae4e561def59dffdbe5d3
mizan <dewan.mizanur911@gmail.com>
2021-12-24 00:17:38 +0600

    users email get api created

--- index.js
+++ index.js
@@ -52,7 +52,17 @@ async function run(){
             const products = await cursor.toArray();
             res.json(products);
         });
-
+        app.get('/users/:email', async (req, res) => {
+            const email = req.params.email;
+            const query = { email: email };
+            const user = await usersCollection.findOne(query);
+            let isAdmin = false;
+            if (user?.role === 'admin') {
+              isAdmin = true;
+            }
+            res.json({ admin: isAdmin });
+          });
+          
         // POST Methods
         app.post('/orders', async(req, res)=>{
             const item = req.body;
