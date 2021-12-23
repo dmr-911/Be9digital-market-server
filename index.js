80836801eda00700d1a4fbb191d730c458b839b0
mizan <dewan.mizanur911@gmail.com>
2021-12-24 00:01:16 +0600

    user/admin put api created

--- index.js
+++ index.js
@@ -104,6 +104,14 @@ async function run(){
             const result = await usersCollection.updateOne(filter, updateDoc, options);
             res.json(result);
           });
+
+          app.put('/users/admin', async (req, res) => {
+            const user = req.body;
+            const filter = { email: user.email };
+            const updateDoc = { $set: { role: 'admin' } };
+            const result = await usersCollection.updateOne(filter, updateDoc);
+            res.json(result);
+          });
     }
     finally{
         // await client.close();
