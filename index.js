25c8cf39d5995a6d125aaad9a1582278e08e4a15
mizan <dewan.mizanur911@gmail.com>
2021-12-24 00:00:24 +0600

    users put method created

--- index.js
+++ index.js
@@ -93,9 +93,17 @@ async function run(){
             res.json(result);
         });
 
-        app.put('/users', async (req, res)=>{
-
-        });
+        app.put('/users', async (req, res) => {
+            const user = req.body;
+            const filter = { email: user.email };
+            const options = { upsert: true };
+      
+            const updateDoc = {
+              $set: user
+            };
+            const result = await usersCollection.updateOne(filter, updateDoc, options);
+            res.json(result);
+          });
     }
     finally{
         // await client.close();
